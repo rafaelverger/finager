9330ee84afd8c562da909bdc009b0176982f776a
Rafael Verger <rafael@rafaelverger.com.br>
2019-12-04 17:17:50 +0000

    Improve DB Schema and OAS definition

--- db/Payment.js
+++ db/Payment.js
@@ -1,7 +1,20 @@
 const mongoose = require('mongoose');
 const MUUID = require('uuid-mongodb');
 
-// TODO couldn't find a way to use swagger (OAS 3.0) schemas as mongoose schemas :(
+//TODO couldn't find a way to use swagger (OAS 3.0) schemas as mongoose schemas :(
+
+const Currency = {
+  type: String,
+  enum: [
+    "GBP",
+    "BRL",
+    "EUR",
+    "USD"
+  ],
+};
+
+const ChargerInformation = mongoose.Schema({ amount: Number, currency: Currency }, { _id : false });
+
 const PaymentSchema = mongoose.Schema({
   _id: { type: String, default: MUUID.v1 },
   attributes: {
@@ -18,28 +31,11 @@ const PaymentSchema = mongoose.Schema({
     },
     charges_information: {
       bearer_code: String,
-      sender_charges: [{
-        amount: Number,
-        currency: {
-          type: String,
-          enum: [
-            "GBP",
-            "BRL",
-            "EUR",
-            "USD"
-          ],
-        }
-      }],
+      receiver_charges_amount: Number,
+      receiver_charges_currency: Currency,
+      sender_charges: [ChargerInformation],
     },
-    currency: {
-      type: String,
-      enum: [
-        "GBP",
-        "BRL",
-        "EUR",
-        "USD"
-      ],
-    },
+    currency: Currency,
     debtor_party: {
       account_name: String,
       account_number: String,
@@ -55,22 +51,14 @@ const PaymentSchema = mongoose.Schema({
       contract_reference: String,
       exchange_rate: Number,
       original_amount: Number,
-      original_currency: {
-        type: String,
-        enum: [
-          "GBP",
-          "BRL",
-          "EUR",
-          "USD"
-        ],
-      }
+      original_currency: Currency
     },
     numeric_reference: Number,
     payment_id: String,
     payment_purpose: String,
     payment_scheme: { type: String, enum: ["FPS"] },
     payment_type: { type: String, enum: ["Credit", "Debit"] },
-    processing_date: String,
+    processing_date: Date,
     reference: String,
     scheme_payment_sub_type: { type: String, enum: ["InternetBanking"] },
     scheme_payment_type: { type: String, enum: ["ImmediatePayment"] },
