92fbe835876f421386842e212073cd193655d0e4
Rafael Verger <rafael@rafaelverger.com.br>
2019-12-05 02:14:11 +0000

    Using a custom micro server to run serverless function in test env

--- tests/_runner.js
+++ tests/_runner.js
@@ -1,8 +1,6 @@
 const { writeFileSync } = require('fs');
 const { spawn } = require('child_process');
 const mongoUnit = require('mongo-unit');
-const request = require('request-promise-native');
-
 
 const [_, __, reportFileDest] = process.argv;
 
@@ -12,51 +10,26 @@ const [_, __, reportFileDest] = process.argv;
  * it avoid the execution of its After hook
  */
 
-let SERVER;
-const startDevServer = (serverLocation) => {
-  console.log('Starting dev server...');
-  SERVER = spawn('now', ['dev', '--listen', serverLocation]);
-  SERVER.on('exit', (code) => {
-    console.log(`[DEV_SERVER] Exited with code: ${code}`);
-  });
-  SERVER.stdout.on('data', (data) => {
-    console.log(`[DEV_SERVER] ${data}`);
-  });
-  SERVER.stderr.on('data', (data) => {
-    console.error(`[DEV_SERVER] ${data}`);
-  });
-}
 
-const checkDevServer = (url, attempts = 15) => {
-  console.log(`[DEBUG] Checking ${url} // attempts left: ${--attempts} ...`);
-  return request
-    .head(url)
-    .catch(err => {
-      if (attempts === 0) {
-        console.error(err);
-        throw err;
-      }
-      return new Promise(r => setTimeout(() => r(checkDevServer(url, attempts)), 500));
-    });
-}
-
+let closeDevServer;
+const startDevServer = require('./_devserver');
 const setup = async () => {
-  const serverLocation = '127.0.0.1:5000';
-  process.env.DEV_SERVER = `http://${serverLocation}`;
   console.log('Starting mongo...');
   await mongoUnit.start();
-  writeFileSync('./.env', `MONGO_URI=${mongoUnit.getUrl()}`);
-  startDevServer(serverLocation);
-  return checkDevServer(process.env.DEV_SERVER)
-    .then(() => console.log('Setup completed.'));
+  process.env.MONGO_URI = mongoUnit.getUrl();
+  return startDevServer()
+    .then(([stopServer, serverUrl]) => {
+      closeDevServer = stopServer;
+      process.env.DEV_SERVER = serverUrl;
+      console.log('Setup completed.')
+    });
 };
 
 const teardown = async () => {
   console.log('Stopping mongo...')
   await mongoUnit.stop();
   console.log('Stopping dev server...');
-  spawn('kill', ['-INT', SERVER.pid]);
-  console.log('Teardown completed.');
+  await closeDevServer().then(() => console.log('Teardown completed.'));
 };
 
 const runTest = () => new Promise((resolve) => {
